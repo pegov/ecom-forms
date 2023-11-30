6b7708a677b8658d4c9ea6cfb32a9842c27aac9f
pegov <dvpegov@gmail.com>
2023-11-30 23:09:08 +0300

    add web app

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from app.template import find_template_name
+from app.web import get_app
 
 t1 = {
     "name": "Template 1",
@@ -14,54 +14,6 @@ t2 = {
     "user_comment": "text",
 }
 
-q1 = {
-    "user_email1": "test@test.com",
-    "user_phone1": "+7 999 888 77 88",
-    "user_date_added": "1997-12-03",
-    "user_comment": "comment1 asdf",
-}
+templates = [t1, t2]
 
-q2 = {
-    "user_email2": "test@test.com",
-    "user_phone2": "+7 999 888 77 88",
-    "user_date_added": "15.08.2003",
-    "user_comment": "comment1 asdf",
-}
-
-q3 = {"user_email1": "text1", "user_phone2": "1997-12-28"}
-
-q4 = {"asdf": "fdfas"}
-
-qs = [
-    (
-        q1,
-        "Template 1",
-    ),
-    (
-        q2,
-        "Template 2",
-    ),
-    (
-        q3,
-        None,
-    ),
-    (
-        q4,
-        None,
-    ),
-]
-
-
-def main():
-    templates = [t1, t2]
-
-    for item in qs:
-        q, result = item[0], item[1]
-
-        name = find_template_name(templates, q)
-        assert name == result
-
-
-if __name__ == "__main__":
-    main()
-    print("success")
+app = get_app(templates)
---
+++ app/web.py
@@ -0,0 +1,50 @@
+from typing import Iterable, Dict, Annotated
+
+from fastapi import FastAPI, Request, Depends, HTTPException
+
+from app.template import find_template_name
+from app.validator import validate
+
+app = FastAPI()
+
+
+def get_templates(request: Request) -> Dict[str, str]:
+    return request.app.state.db
+
+
+TemplatesDep = Annotated[Iterable[Dict[str, str]], Depends(get_templates)]
+
+
+# f_name1=value1&f_name2=value2
+# I assume it is raw body, not JSON.
+# So we need to split raw body by '&' and items by '=' to get data.
+@app.post("/get_form")
+async def get_form(request: Request, templates: TemplatesDep):
+    body = (await request.body()).decode()
+    items = {}
+    for item in body.split("&"):
+        key, value = item.split("=", 1)
+        if len(key) == 0:
+            raise HTTPException(400, detail="field name is empty")
+
+        items.update({key: value})
+
+    if len(items) == 0:
+        raise HTTPException(400, detail="no data")
+
+    name = find_template_name(templates, items)
+
+    if name is not None:
+        return {"name": name}
+
+    res = {}
+    for key, value in items.items():
+        type_ = validate(value)
+        res.update({key: type_})
+
+    return res
+
+
+def get_app(db: Iterable[dict]) -> FastAPI:
+    app.state.db = db
+    return app
