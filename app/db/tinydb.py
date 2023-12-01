b6f48383d1f9137b9184f23ee659a6b9eefefad5
pegov <dvpegov@gmail.com>
2023-12-01 17:20:24 +0300

    fix template search algorithm

--- app/db/tinydb.py
+++ app/db/tinydb.py
@@ -1,6 +1,6 @@
 from typing import Dict, Optional
 
-from tinydb import TinyDB, Query
+from tinydb import TinyDB
 
 from app.db.abc import AbstractDatabase
 
@@ -12,8 +12,23 @@ class TinyDBDatabase(AbstractDatabase):
         self._db = db
 
     def get_template_name(self, fields: Dict[str, str]) -> Optional[str]:
-        templates = self._db.search(Query().fragment(fields))
-        if len(templates) == 0:
+        candidate_name = ""
+        candidate_len = 0
+        for template in self._db:
+            template_len = len(template) - 1
+
+            if template_len > len(fields):
+                continue
+
+            name = template.pop("name")
+            if (
+                set(template.items()).issubset(set(fields.items()))
+                and template_len > candidate_len
+            ):
+                candidate_name = name
+                candidate_len = template_len
+
+        if candidate_len == 0:
             return None
 
-        return max(templates, key=lambda x: len(x)).get("name")
+        return candidate_name
